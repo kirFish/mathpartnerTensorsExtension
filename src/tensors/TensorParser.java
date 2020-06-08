63fcd52998d1dcd664d39ecbd5c576bc2753a667
kirichripper <kir.fishchuk2014@gmail.com>
2020-06-08 17:11:54 +0300

    repository init

---
+++ src/tensors/TensorParser.java
@@ -0,0 +1,14 @@
+package tensors;
+
+import java.util.Map;
+
+public class TensorParser {
+
+    public void inputParser(){
+
+    }
+
+    public Map<String, String> outputParser(){
+        return null;
+    }
+}
